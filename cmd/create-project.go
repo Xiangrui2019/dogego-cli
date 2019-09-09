049bd2d256194dd89126c4dcd7a2957395ac5269
Go Go Go <xiangrui@aiursoft.com>
2019-09-09 19:31:10 +0800

    add create-project

--- cmd/create-project.go
+++ cmd/create-project.go
@@ -2,6 +2,10 @@ package cmd
 
 import (
 	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -15,8 +19,82 @@ var CreateProject = &cobra.Command{
 	Run:   createProject,
 }
 
+func replaceProjectName() {
+	err := filepath.Walk(project_name, func(path string, f os.FileInfo, err error) error {
+		var rb string
+
+		if f == nil {
+			return err
+		}
+		if f.IsDir() {
+			return nil
+		}
+
+		buffer := make([]byte, f.Size())
+
+		fp, _ := os.Open(path)
+
+		fp.Read(buffer)
+		fp.Close()
+
+		if tmpl_type == "mini" {
+			rb = strings.Replace(string(buffer), "dogego-mini", project_name, -1)
+		} else {
+			rb = strings.Replace(string(buffer), "dogego", project_name, -1)
+		}
+
+		fp, _ = os.OpenFile(path, os.O_WRONLY, 666)
+		fp.WriteString(rb)
+		fp.Close()
+		log.Printf("处理: %s", path)
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func createProject(cmd *cobra.Command, args []string) {
-	log.Println(project_name)
+	var cmdx *exec.Cmd
+
+	if tmpl_type == "" {
+		tmpl_type = "full"
+	}
+
+	log.Println("创建项目中.........")
+
+	if tmpl_type == "mini" {
+		cmdx = exec.Command(
+			"git",
+			"clone",
+			"https://github.com/Xiangrui2019/dogego",
+			project_name)
+	} else {
+		cmdx = exec.Command(
+			"git",
+			"clone",
+			"https://github.com/Xiangrui2019/dogego-mini",
+			project_name)
+	}
+	wd, _ := os.Getwd()
+
+	cmdx.Stdout = os.Stdout
+	cmdx.Dir = wd
+	err := cmdx.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = exec.Command("rm", "-rf", project_name+"/.git").Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	replaceProjectName()
 }
 
 func init() {
