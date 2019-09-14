31177e82d927611d0efe6952e64cfa68245d67ab
Go Go Go <xiangrui@aiursoft.com>
2019-09-14 16:54:34 +0800

    replacer lize

--- cmd/create-project.go
+++ cmd/create-project.go
@@ -19,10 +19,60 @@ var CreateProject = &cobra.Command{
 	Run:   createProject,
 }
 
+func defaultValue() {
+	if tmpl_type == "" {
+		tmpl_type = "full"
+	}
+}
+
+func ProjectTypeGit(workdir string) *exec.Cmd {
+	var gitCommand *exec.Cmd
+
+	switch tmpl_type {
+	case "mini":
+		gitCommand = exec.Command(
+			"git",
+			"clone",
+			"https://github.com/Xiangrui2019/dogego-mini",
+			project_name)
+	case "jrpc":
+		gitCommand = exec.Command(
+			"git",
+			"clone",
+			"https://github.com/Xiangrui2019/jrpc",
+			project_name)
+	default:
+		gitCommand = exec.Command(
+			"git",
+			"clone",
+			"https://github.com/Xiangrui2019/dogego",
+			project_name)
+	}
+
+	gitCommand.Stdout = os.Stdout
+	gitCommand.Stderr = os.Stdout
+	gitCommand.Stdin = os.Stdin
+	gitCommand.Dir = workdir
+
+	return gitCommand
+}
+
+func replacer(s string) string {
+	var result string
+
+	switch tmpl_type {
+	case "mini":
+		result = strings.Replace(s, "dogego-mini", project_name, -1)
+	case "jrpc":
+	default:
+		result = strings.Replace(s, "dogego", project_name, -1)
+	}
+
+	return result
+}
+
 func replaceProjectName() {
 	err := filepath.Walk(project_name, func(path string, f os.FileInfo, err error) error {
-		var rb string
-
 		if f == nil {
 			return err
 		}
@@ -32,21 +82,19 @@ func replaceProjectName() {
 
 		buffer := make([]byte, f.Size())
 
-		fp, _ := os.Open(path)
-
+		fp, err := os.Open(path)
 		fp.Read(buffer)
 		fp.Close()
 
-		if tmpl_type == "mini" {
-			rb = strings.Replace(string(buffer), "dogego-mini", project_name, -1)
-		} else {
-			rb = strings.Replace(string(buffer), "dogego", project_name, -1)
-		}
+		result := replacer(string(buffer))
 
-		fp, _ = os.OpenFile(path, os.O_WRONLY|os.O_TRUNC, 666)
-		fp.WriteString(rb)
-		fp.Close()
-		log.Printf("处理: %s", path)
+		fp, err = os.OpenFile(path, os.O_WRONLY|os.O_TRUNC, 666)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			fp.WriteString(result)
+			log.Printf(path)
+		}
 
 		return nil
 	})
@@ -57,41 +105,17 @@ func replaceProjectName() {
 }
 
 func createProject(cmd *cobra.Command, args []string) {
-	var cmdx *exec.Cmd
+	defaultValue()
 
-	if tmpl_type == "" {
-		tmpl_type = "full"
-	}
+	workdir, _ := os.Getwd()
+	command := ProjectTypeGit(workdir)
 
-	log.Println("创建项目中.........")
-
-	if tmpl_type == "mini" {
-		cmdx = exec.Command(
-			"git",
-			"clone",
-			"https://github.com/Xiangrui2019/dogego-mini",
-			project_name)
-	} else {
-		cmdx = exec.Command(
-			"git",
-			"clone",
-			"https://github.com/Xiangrui2019/dogego",
-			project_name)
-	}
-	wd, _ := os.Getwd()
-
-	cmdx.Stdout = os.Stdout
-	cmdx.Stderr = os.Stdout
-	cmdx.Stdin = os.Stdin
-	cmdx.Dir = wd
-	err := cmdx.Run()
-
+	err := command.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	err = exec.Command("rm", "-rf", project_name+"/.git").Run()
-
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -102,5 +126,5 @@ func createProject(cmd *cobra.Command, args []string) {
 func init() {
 	CreateProject.Flags().StringVarP(&project_name, "name", "n", "", "项目名称")
 	CreateProject.MarkFlagRequired("name")
-	CreateProject.Flags().StringVarP(&tmpl_type, "type", "t", "", "项目类型(full or mini)")
+	CreateProject.Flags().StringVarP(&tmpl_type, "type", "t", "", "项目类型(full or mini or jrpc or jrpc+orm)")
 }
